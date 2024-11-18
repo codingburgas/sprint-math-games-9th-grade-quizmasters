abd52aac758f7ca988b2f991836fe3de22f33e63
SAVassilev23 <SAVassilev23@codingburgas.bg>
2024-11-18 20:49:24 +0200

    Make Deck changes

--- BlackJack/lib/precompile.h
+++ BlackJack/lib/precompile.h
@@ -1,3 +1,6 @@
 #pragma once
 #include <iostream>
 #include <raylib.h>
+#include <string>
+#include <vector>
+#include <random>
---
+++ BlackJack/src/Deck.cpp
@@ -0,0 +1,7 @@
+#include "../lib/Deck.h"
+
+Cards::Cards(int suit,int rank,std::string problem); {
+    m_suit=suit;
+    m_rank=rank;
+    m_problem=problem;
+}
